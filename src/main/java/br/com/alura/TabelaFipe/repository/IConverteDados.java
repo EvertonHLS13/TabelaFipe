fca74ca115d124a0f9cfabe67c02c823b452b28f
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2025-11-01 23:36:25 -0300

    Add files via upload

--- src/main/java/br/com/alura/TabelaFipe/repository/IConverteDados.java
+++ src/main/java/br/com/alura/TabelaFipe/repository/IConverteDados.java
@@ -1,7 +1,11 @@
 package br.com.alura.TabelaFipe.repository;
 
+import java.util.List;
+
 public interface IConverteDados {
 
     <T> T obeterDados(String json, Class<T>  classe);
 
+
+    <T>List<T> obterLista(String json, Class<T> classe);
 }
--- src/main/java/br/com/alura/TabelaFipe/service/ConverteDados.java
+++ src/main/java/br/com/alura/TabelaFipe/service/ConverteDados.java
@@ -3,6 +3,10 @@ package br.com.alura.TabelaFipe.service;
 import br.com.alura.TabelaFipe.repository.IConverteDados;
 import org.springframework.boot.json.JsonParseException;
 import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.type.CollectionType;
+
+import java.util.Collection;
+import java.util.List;
 
 public class ConverteDados implements IConverteDados {
     private ObjectMapper maper = new ObjectMapper();
@@ -16,4 +20,18 @@ public class ConverteDados implements IConverteDados {
             throw  new RuntimeException(e);
         }
     }
-}
+
+        @Override
+        public <T> List<T> obterLista(String json, Class<T> classe) {
+            CollectionType lista = maper.getTypeFactory()
+                    .constructCollectionType(List.class, classe);
+
+            try {
+                return maper.readValue(json, lista);
+            } catch (JsonParseException e){
+                throw new RuntimeException(e);
+            }
+
+            }
+
+    }
